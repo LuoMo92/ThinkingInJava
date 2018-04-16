7aefd0a6e53854de6b077dfe44b544203513948c
luomo92 <810754420@qq.com>
2018-04-16 21:58:49 +0800

    第三章P93练习一

---
+++ src/main/java/com/luomo/tij/chapter3/PrintTest.java
@@ -0,0 +1,18 @@
+package com.luomo.tij.chapter3;
+
+import static net.mindview.util.Print.print;
+
+/**
+ * 中文版
+ * P93 练习一
+ * @author LiuMei
+ * @date 2018-04-16
+ */
+public class PrintTest {
+
+    public static void main(String[] args) {
+        print("简短的打印语句");
+        System.out.println("正常的打印语句");
+    }
+
+}
