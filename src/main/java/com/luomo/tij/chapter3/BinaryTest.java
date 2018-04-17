4215c0514269198b4601d22fd37fcde72157d469
luomo92 <810754420@qq.com>
2018-04-17 22:04:59 +0800

    第三章练习

---
+++ src/main/java/com/luomo/tij/chapter3/BinaryTest.java
@@ -0,0 +1,23 @@
+package com.luomo.tij.chapter3;
+
+import static net.mindview.util.Print.print;
+
+/**
+ * @author LiuMei
+ * @date 2018-04-17
+ */
+public class BinaryTest {
+
+    public static void main(String[] args) {
+        int i = 1 + 4 + 16 + 64;
+        int j = 2 + 8 + 32 + 128;
+        print("i = " + Integer.toBinaryString(i));
+        print("j = " + Integer.toBinaryString(j));
+        print("i & j = " + Integer.toBinaryString(i & j));
+        print("i | j = " + Integer.toBinaryString(i | j));
+        print("i ^ j = " + Integer.toBinaryString(i ^ j));
+        print("~i = " + Integer.toBinaryString(~i));
+        print("~j = " + Integer.toBinaryString(~j));
+    }
+
+}
---
+++ src/main/java/com/luomo/tij/chapter3/RightShiftTest2.java
@@ -0,0 +1,22 @@
+package com.luomo.tij.chapter3;
+
+import static net.mindview.util.Print.print;
+
+/**
+ * @author LiuMei
+ * @date 2018-04-17
+ */
+public class RightShiftTest2 {
+
+    public static void main(String[] args) {
+        int h = -1;
+        print(Integer.toBinaryString(h));
+        h <<= 10;
+        print(Integer.toBinaryString(h));
+        for (int i = 0; i < 32; i++) {
+            h >>>= 1;
+            print(Integer.toBinaryString(h));
+        }
+    }
+
+}
